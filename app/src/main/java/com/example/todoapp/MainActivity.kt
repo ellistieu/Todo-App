a9736f8856ecdeb9f8b3187dce718e3e3fec4068
ellistieu <80673895+ellistieu@users.noreply.github.com>
2023-01-15 16:45:16 -0500

    Added New Toolbar and Implemented Light/Dark Mode Button

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.LineNumberReader
@@ -13,11 +16,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Recycler View
         todoAdapter = TodoAdapter(mutableListOf())
 
         rvTodoItems.adapter = todoAdapter
         rvTodoItems.layoutManager = LinearLayoutManager(this)
 
+        // TODO Menu button
+        val btnMenu = findViewById<ImageView>(R.id.btnMenu)
+
+        // Light/Dark Button
+        val switchLd = findViewById<ImageView>(R.id.switchLd)
+        var isDarkMode = false
+        switchLd.setOnClickListener(View.OnClickListener {
+            if(isDarkMode){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            isDarkMode = !isDarkMode
+        })
+
+        // Add todos
         btnAddTodo.setOnClickListener {
             val todoTitle = etTodoTitle.text.toString()
             if(todoTitle.isNotEmpty()) {
@@ -26,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                 etTodoTitle.text.clear()
             }
         }
+        // Delete todos which are struck through
         btnDeleteTodos.setOnClickListener {
             todoAdapter.deleteDoneTodos()
         }
